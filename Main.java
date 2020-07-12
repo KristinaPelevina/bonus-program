b907584e399c59e076ad2bb60cdae666e5d9933f
Kristina Pelevina <christya_13.07@mail.ru>
2020-07-12 21:40:15 +0600

    Main changed

--- Main.java
+++ Main.java
@@ -1,12 +1,17 @@
-    public class Main {
-        public static void main(String[] args) {
-            long balance = 500_000_500;
-            long transfer = 700_000_100_000L;
-            long bonus = 1;
-            long rubles = 100;
-
-            long amount = balance + transfer + transfer / rubles * bonus;
-            System.out.println(amount);
+public class Main {
+    public static void main(String[] args) {
+        long transfer = 1100L;
+        long bonus;
+        long rubles = 100;
+        long condition = 1000;
+        if (transfer >= condition) {
+            bonus = transfer / 100;
+        } else {
+            bonus = 0;
         }
-    }
 
+        long balance = 500_000_100;
+        long amount = balance + transfer + bonus;
+        System.out.println(amount);
+    }
+}
